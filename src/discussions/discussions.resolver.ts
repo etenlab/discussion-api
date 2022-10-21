fb8063e0360d6e4716a16c5b3e5517dfbfb4da0e
Hiroshi <hiroshi.tan57@gmail.com>
2022-10-21 03:36:14 -0400

    remove subscription in discussion resolver

--- src/discussions/discussions.resolver.ts
+++ src/discussions/discussions.resolver.ts
@@ -1,15 +1,14 @@
 import { NotFoundException } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver, Subscription, Int } from '@nestjs/graphql';
-import { PubSub } from 'graphql-subscriptions';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { Discussion } from './discussion.model';
 import { DiscussionsService } from './discussions.service';
 import { NewDiscussionInput } from './new-discussion.input';
 
-const pubSub = new PubSub();
-
 @Resolver(of => Discussion)
 export class DiscussionsResolver {
-  constructor(private readonly discussionsService: DiscussionsService) {}
+  constructor(
+    private readonly discussionsService: DiscussionsService,
+  ) {}
 
   @Query(returns => Discussion)
   async discussion(@Args('id') id: number): Promise<Discussion> {
@@ -25,17 +24,11 @@ export class DiscussionsResolver {
     @Args('newDiscussionData') newDiscussionData: NewDiscussionInput,
   ): Promise<Discussion> {
     const discussion = await this.discussionsService.create(newDiscussionData);
-    pubSub.publish('discussionAdded', { discussionAdded: discussion });
     return discussion;
   }
 
   @Mutation(returns => Boolean)
   async removeDiscussion(@Args('id') id: number) {
     return this.discussionsService.remove(id);
-  }
-
-  @Subscription(returns => Discussion)
-  discussionAdded() {
-    return pubSub.asyncIterator('discussionAdded');
   }
 }
